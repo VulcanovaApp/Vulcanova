e57b4625ec93bb435bca43c55c3be2dc304a4673
MRmlik12 <mrmlik12sup@gmail.com>
2022-03-22 21:25:37 +0100

    Add homeworks api implementation

---
+++ Vulcanova.Uonet/Api/Homeworks/GetHomeworksByPupilQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vulcanova.Uonet.Api.Homeworks
+{
+    public record GetHomeworksByPupilQuery(
+        int PupilId,
+        int PeriodNumber,
+        DateTime LastSyncDate,
+        int LastId = int.MinValue,
+        int PageSize = 500): IApiQuery<HomeworkPayload[]>
+    {
+        public const string ApiEndpoint = "mobile/homework/byPupil";
+    }
+}
---
+++ Vulcanova.Uonet/Api/Homeworks/HomeworkPayload.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Vulcanova.Uonet.Api.Common.Models;
+
+namespace Vulcanova.Uonet.Api.Homeworks
+{
+    public class HomeworkPayload
+    {
+        public long Id { get; set; }
+        public Guid Key { get; set; }
+        public long IdPupil { get; set; }
+        public long IdHomework { get; set; }
+        public string Content { get; set; }
+        public bool IsAnswerRequired { get; set; }
+        public AnswerDeadline DateCreated { get; set; }
+        public AnswerDeadline Date { get; set; }
+        public AnswerDeadline Deadline { get; set; }
+        public object AnswerDate { get; set; }
+        public AnswerDeadline AnswerDeadline { get; set; }
+        public Teacher Creator { get; set; }
+        public Subject Subject { get; set; }
+        public List<object> Attachments { get; set; }
+    }
+
+    public class AnswerDeadline
+    {
+        public long Timestamp { get; set; }
+        public Date Date { get; set; }
+        public string DateDisplay { get; set; }
+        public Date Time { get; set; }
+    }
+}
