2004420957a3b83f2275200c6a046c6942440a5c
MRmlik12 <mrmlik12sup@gmail.com>
2022-03-24 16:31:09 +0100

    Replace AnswerDeadline object with Date in HomePayload

--- Vulcanova.Uonet/Api/Homeworks/HomeworkPayload.cs
+++ Vulcanova.Uonet/Api/Homeworks/HomeworkPayload.cs
@@ -12,21 +12,13 @@ namespace Vulcanova.Uonet.Api.Homeworks
         public long IdHomework { get; set; }
         public string Content { get; set; }
         public bool IsAnswerRequired { get; set; }
-        public AnswerDeadline DateCreated { get; set; }
-        public AnswerDeadline Date { get; set; }
-        public AnswerDeadline Deadline { get; set; }
+        public Date DateCreated { get; set; }
+        public Date Date { get; set; }
+        public Date Deadline { get; set; }
         public object AnswerDate { get; set; }
-        public AnswerDeadline AnswerDeadline { get; set; }
+        public Date AnswerDeadline { get; set; }
         public Teacher Creator { get; set; }
         public Subject Subject { get; set; }
         public List<object> Attachments { get; set; }
-    }
-
-    public class AnswerDeadline
-    {
-        public long Timestamp { get; set; }
-        public Date Date { get; set; }
-        public string DateDisplay { get; set; }
-        public Date Time { get; set; }
     }
 }
